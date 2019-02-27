ae25a6127daa57306951e89a1d22ec50b0b27d46
John Rigler <john@rigler.org>
2019-02-26 20:17:25 -0600

    unpack.bash

--- unpack.bash
+++ unpack.bash
@@ -1,1 +1,12 @@
-./unspendable.py HTTPScssTiNYURLdCoMsjrigLer D 30
+./unspendable.py TiNYURLdCoMsJRiGLERz D 30 | while read LINE
+do
+   echo $LINE
+   LINE=$(echo $LINE | sed 's/c/\:/g');
+   LINE=$(echo $LINE | sed 's/s/\//g');
+   LINE=$(echo $LINE | sed 's/d/\./g');
+   LINE=$(echo $LINE | sed 's/h/\http/g');
+   LINE=$(echo $LINE | sed 's/c/\:/g');
+   LINE=$(echo $LINE | sed 's/c/\:/g');
+   LINE=$(echo $LINE | sed 's/z.*//g');
+   echo $LINE | cut -c 2- | tr 'A-Z' 'a-z'
+done 
