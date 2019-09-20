f252f5d523d8b371a667c4c22ffa2292224af82c
John Rigler <john@rigler.org>
2019-09-20 23:33:00 +0000

    make test understand environment

--- test/test.bash
+++ test/test.bash
@@ -1,2 +1,7 @@
-env
-/home/circleci/project/unspendable.py D DDDDzzzzzzzzzzzzzzzzzzzzzzzz 30 | grep E4X3tx
+if [[ CIRCLE_CI == 'true' ]]
+then
+DIR=/home/circleci/project
+else
+DIR=/root/unspendable
+fi
+$DIR/unspendable.py D DDDDzzzzzzzzzzzzzzzzzzzzzzzz 30 | grep E4X3tx
