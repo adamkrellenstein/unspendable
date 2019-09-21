44d718224924ed97bc04f55126c40a81811501b3
John Rigler <john@rigler.org>
2019-09-21 01:00:59 +0000

    .

--- test/test.bash
+++ test/test.bash
@@ -4,4 +4,4 @@ DIR=/home/circleci/project
 else
 DIR=/root/unspendable
 fi
-/home/circleci/project/unspendable.py D DDDDzzzzzzzzzzzzzzzzzzzzzzzz 30 | grep E4X3tx
+$DIR/unspendable.py D DDDDzzzzzzzzzzzzzzzzzzzzzzzz 30 | grep E4X3tx
