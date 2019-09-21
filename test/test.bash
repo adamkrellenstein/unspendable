37c3cab2bffcc0ed7c926c86d8e14d543ca2176e
John Rigler <john@rigler.org>
2019-09-21 22:49:50 +0000

    changed simple sum

--- test/test.bash
+++ test/test.bash
@@ -13,4 +13,4 @@ for X in 1 2 3 4 5 6 7 8 9s 9t 9u 9v 9w 9x 9y 9z A B C D DC DCx E F G H J K mv
 do
 $CMD $X $T 
 done  } 
-unspendable_test | sum | grep 02023 
+unspendable_test | sum | grep 30341 
