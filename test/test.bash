73eb450747f95afdf85b8d0e744c0a0ae317d6c1
John Rigler <john@rigler.org>
2019-09-21 22:54:22 +0000

    .

--- test/test.bash
+++ test/test.bash
@@ -8,9 +8,9 @@ DIR=/root/unspendable
 fi
 
 CMD=$DIR/unspendable.py
-T=DDxxDDDDDDDDD
+T="DD  DDDDDDDDD"
 for X in 1 2 3 4 5 6 7 8 9s 9t 9u 9v 9w 9x 9y 9z A B C D DC DCx E F G H J K mv
 do
-$CMD $X $T 
+$CMD $X "$T"
 done  } 
-unspendable_test | sum | grep 30341 
+unspendable_test | sum | grep 02023
